4b3b3aba842e6980b6a919e307c27ac79ccc3129
katuskaa <kfabianova11@gmail.com>
2019-01-06 03:05:28 +0100

    changed time zone to utc

--- app/src/main/java/diabetes/com/synchronization/common/helpers/DateAndTimeHelper.kt
+++ app/src/main/java/diabetes/com/synchronization/common/helpers/DateAndTimeHelper.kt
@@ -3,9 +3,10 @@ package diabetes.com.synchronization.common.helpers
 import java.text.SimpleDateFormat
 import java.util.*
 
-private val TIME_FORMAT__ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+private val TIME_FORMAT__ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.000'Z'"
 
 fun getISO_8601Date(): String {
     val dateFormat = SimpleDateFormat(TIME_FORMAT__ISO_8601)
+    dateFormat.timeZone = TimeZone.getTimeZone("UTC");
     return dateFormat.format(Date())
 }
