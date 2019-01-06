e0a27075e9748c7cf274fd7a69f52aef5cb02395
katuskaa <kfabianova11@gmail.com>
2019-01-06 01:46:57 +0100

    changed date format

--- app/src/main/java/diabetes/com/synchronization/communication/network/treatments/getTreatments/GetTreatmentsResponseBody.kt
+++ app/src/main/java/diabetes/com/synchronization/communication/network/treatments/getTreatments/GetTreatmentsResponseBody.kt
@@ -1,6 +1,5 @@
 package diabetes.com.synchronization.communication.network.treatments.getTreatments
 
 import com.google.gson.annotations.SerializedName
-import java.util.*
 
-open class GetTreatmentsResponseBody(@SerializedName("_id") var id: String, @SerializedName("enteredBy") var enteredBy: String, @SerializedName("eventType") var eventType: String, @SerializedName("reason") var reason: String, @SerializedName("carbs") var carbs: Int, @SerializedName("duration") var duration: Int, @SerializedName("created_at") var createdAt: Date, @SerializedName("insulin") var insulin: Float, @SerializedName("notes") var notes: String)
+open class GetTreatmentsResponseBody(@SerializedName("_id") var id: String, @SerializedName("enteredBy") var enteredBy: String, @SerializedName("eventType") var eventType: String, @SerializedName("reason") var reason: String, @SerializedName("carbs") var carbs: Int, @SerializedName("duration") var duration: Int, @SerializedName("created_at") var createdAt: String, @SerializedName("insulin") var insulin: Float, @SerializedName("notes") var notes: String)
